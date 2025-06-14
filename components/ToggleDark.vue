0a145ff36c690952c5eec7a2a3c5423ead15f590
Baghaztra <baghaztra21@gmail.com>
2025-06-14 14:05:07 +0700

    dark mode bug fixed

--- components/ToggleDark.vue
+++ components/ToggleDark.vue
@@ -1,23 +1,30 @@
 <script setup>
-const isDark = ref(
-  localStorage.getItem("theme") === "dark"
-    ? true
-    : localStorage.getItem("theme") === "light"
-    ? false
-    : window.matchMedia("(prefers-color-scheme: dark)").matches
-);
+import { ref, onMounted } from "vue";
+
+const isDark = ref(false);
 
 onMounted(() => {
-  if (localStorage.getItem("theme") === "dark") {
-    isDark.value = true;
-    document.documentElement.classList.add("dark");
+  if (process.client) {
+    const savedTheme = localStorage.getItem("theme");
+    isDark.value =
+      savedTheme === "dark"
+        ? true
+        : savedTheme === "light"
+        ? false
+        : window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (isDark.value) {
+      document.documentElement.classList.add("dark");
+    }
   }
 });
 
 function toggleDark() {
   isDark.value = !isDark.value;
-  document.documentElement.classList.toggle("dark");
-  localStorage.setItem("theme", isDark.value ? "dark" : "light");
+  if (process.client) {
+    document.documentElement.classList.toggle("dark");
+    localStorage.setItem("theme", isDark.value ? "dark" : "light");
+  }
 }
 </script>
 
@@ -25,11 +32,13 @@ function toggleDark() {
   <button
     @click="toggleDark"
     class="relative inline-flex items-center h-6 rounded-full w-11 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white/50"
-    :class="isDark ? 'bg-white/30' : 'bg-white/20'">
-    <span class="sr-only">Toggle dark mode</span>
+    :class="isDark ? 'bg-white/30' : 'bg-white/20'"
+  >
+    <span class="sr-only">Ganti tema gelap</span>
     <span
       class="absolute left-0 w-5 h-5 transform bg-white rounded-full transition-transform duration-300 flex items-center justify-center text-gray-800"
-      :class="{ 'translate-x-5': isDark, 'translate-x-0': !isDark }">
+      :class="{ 'translate-x-5': isDark, 'translate-x-0': !isDark }"
+    >
       <Icon :name="isDark ? 'lucide-moon' : 'lucide-sun'" class="text-sm" />
     </span>
   </button>
